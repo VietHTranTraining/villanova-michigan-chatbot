27b45d82666077f3b11369c3bd8dee21e3471862
Viet Hung Tran <viethtran1620@gmail.com>
2018-04-08 18:40:19 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import json
 from consts import PLAYERS
+from ibm_utils import startup_discovery
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def searchTeams():
-    return 'gg'
+    return render_template('index.html')
 
 if __name__ == "__main__":
+    startup_discovery()
     app.secret_key = "super_secret_key"
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
