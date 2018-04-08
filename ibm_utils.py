999253008fd97f4e3683065b61f065efc1cd6145
Viet Hung Tran <viethtran1620@gmail.com>
2018-04-08 16:58:43 -0400

    Update ibm_utils.py

--- ibm_utils.py
+++ ibm_utils.py
@@ -2,6 +2,7 @@ from watson_developer_cloud import DiscoveryV1
 from config import DISCOVERY_USERNAME, DISCOVERY_PASSWORD
 from env_config import ENV_CONFIG
 import json
+import os
 
 discovery = DiscoveryV1(
     username = DISCOVERY_USERNAME,
@@ -12,6 +13,7 @@ discovery = DiscoveryV1(
 ENV_NAME = 'mm-env'
 CONFIG_NAME = 'mm-config'
 COLLECTION_NAME = 'mm-collection'
+ARTICLES_DIR = './articles'
 ENV_ID = None
 CONFIG_ID = None
 COLLECTION_ID = None
@@ -62,6 +64,7 @@ def list_collections():
         print("list_collections error: Configuration ID invalid or not yet set")
         return None 
     collections = discovery.list_collections(ENV_ID)
+    return collections
 
 def delete_env():
     del_env = discovery.delete_environment(ENV_ID)
@@ -72,7 +75,7 @@ def delete_config():
 def delete_collection():
     del_collection = discovery.delete_collection(ENV_ID, COLLECTION_ID)
 
-def create_env():
+def get_env():
     global ENV_ID
     envs = list_envs()
     if envs is not None:
@@ -80,6 +83,40 @@ def create_env():
             if env["name"] == ENV_NAME:
                 ENV_ID = env['environment_id']
                 return True
+    return False
+
+def get_config():
+    global CONFIG_ID
+    if not validate_env():
+        print("create_config error: Environment ID invalid or not yet set")
+        return False
+    configs = list_configs()
+    if configs is not None:
+        for config in configs['configurations']:
+            if config["name"] == CONFIG_NAME:
+                CONFIG_ID = config['configuration_id']
+                return True
+    return False
+
+def get_collection():
+    global COLLECTION_ID
+    if not validate_env():
+        print("create_collection error: Environment ID invalid or not yet set")
+        return False
+    elif not validate_config():
+        print("create_collection error: Configuration ID invalid or not yet set")
+        return False
+    collections = list_collections()
+    if collections is not None:
+        for collection in collections['collections']:
+            if collection["name"] == COLLECTION_NAME:
+                COLLECTION_ID = collection['collection_id']
+                return True
+
+def create_env():
+    global ENV_ID
+    if get_env():
+        return True
     new_env = None
     try:
         new_env = discovery.create_environment(
@@ -94,15 +131,8 @@ def create_env():
 
 def create_config():
     global CONFIG_ID
-    if not validate_env():
-        print("create_config error: Environment ID invalid or not yet set")
-        return False
-    configs = list_configs()
-    if configs is not None:
-        for config in configs['configurations']:
-            if config["name"] == CONFIG_NAME:
-                CONFIG_ID = config['configuration_id']
-                return True
+    if get_config():
+        return True
     new_config = None
     try:
         new_config = discovery.create_configuration(ENV_ID,  
@@ -119,18 +149,8 @@ def create_config():
 
 def create_collection():
     global COLLECTION_ID
-    if not validate_env():
-        print("create_collection error: Environment ID invalid or not yet set")
-        return False
-    elif not validate_config():
-        print("create_collection error: Configuration ID invalid or not yet set")
-        return False
-    collections = list_collections()
-    if collections is not None:
-        for collection in collections['collections']:
-            if collection["name"] == COLLECTION_NAME:
-                COLLECTION_ID = collection['collection_id']
-                return True
+    if get_collection():
+        return True
     new_collection = None
     try:
         new_collection = discovery.create_collection(environment_id = ENV_ID, 
@@ -144,14 +164,39 @@ def create_collection():
     COLLECTION_ID = new_collection['collection_id']
     return True
 
-def startup():
-    if not create_env():
-        print("Create environment failed")
-    elif not create_config():
-        print("Create configuration failed")
-    elif not create_collection():
-        print("Create collection failed")
+def add_doc(file_name):
+    if not validate_env():
+        print("add_doc error: Environment ID invalid or not yet set")
+        return False
+    elif not validate_collection():
+        print("add_doc error: Collection ID invalid or not yet set")
+        return False
+    try:
+        with open(os.path.join(os.getcwd(), file_name)) as fileinfo:
+              add_doc = discovery.add_document(ENV_ID, COLLECTION_ID, file=fileinfo)
+              print(json.dumps(add_doc, indent=2))
+    except Exception as ex:
+        print(str(ex))
+        return False
+    return True
+
+def add_all_docs():
+    print("Adding documents...")
+    file_names = [os.path.join(ARTICLES_DIR, fl) 
+                  for fl in os.listdir(ARTICLES_DIR) 
+                    if os.path.isfile(os.path.join(ARTICLES_DIR, fl))]
+    for file_name in file_names:
+        if not add_doc(file_name):
+            print "Add file " + file_name + " failed"
+            return False
+    return True
+
+def startup_discovery():
+    if not get_env():
+        print("Get environment failed")
+    elif not get_config():
+        print("Get configuration failed")
+    elif not get_collection():
+        print("Get collection failed")
     else:
         print("Startup success")
-
-startup()
